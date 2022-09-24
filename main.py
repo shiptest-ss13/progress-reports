5eae6b4e3762c9b3c20795df1a74b6e937f60a53
Mark Suckerberg <29362068+MarkSuckerberg@users.noreply.github.com>
2022-09-23 20:44:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import mistletoe
 import os
 
 with open("output/index.html", "x") as out:
-    for file in os.listdir("reports").reverse():
+    filelist = os.listdir("reports")
+    for file in filelist.reverse():
         with open(f"reports/{file}") as f:
             out.write(mistletoe.markdown(f))
         out.write("<br>\n<hr>\n<br>\n")
