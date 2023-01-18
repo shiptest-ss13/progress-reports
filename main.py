0b903dd2ddf5003573ea3607c1e3b2bdf385e298
Mark Suckerberg <29362068+MarkSuckerberg@users.noreply.github.com>
2023-01-18 00:07:26 -0600

    Guess we didn't need that anymore

--- main.py
+++ main.py
@@ -11,10 +11,6 @@ def WriteReport(target, input_filename):
 with open("output/index.html", "x") as out:
 	with open("output/all.html", "x") as out_all:
 		filelist = os.listdir("reports")
-		#We want the latest first, so it'll be the last alphabetically if numbered ascendingly
-		#filelist.reverse()
-
-		print(enumerate(filelist))
 
 		for index, file in enumerate(filelist):
 			#Only write the first three reports to the index file
