b7135eac885578bc8fa390ff3f12e4e258852afb
Mark Suckerberg <29362068+MarkSuckerberg@users.noreply.github.com>
2023-01-17 23:59:54 -0600

    I'll get it one of these times

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def WriteReport(target, input_filename):
 	target.write("</div>\n<br><hr><br>\n")
 
 with open("output/index.html", "x") as out:
-	with open("output/index.html", "x") as out_all:
+	with open("output/all.html", "x") as out_all:
 		filelist = os.listdir("reports")
 		#We want the latest first, so it'll be the last alphabetically if numbered ascendingly
 		filelist.reverse()
